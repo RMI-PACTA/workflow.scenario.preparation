c3dd342a5312681d61a113bbb9fc1900623bc9d8
CJ Yetman <cyetman@rmi.org>
2024-04-11 08:23:00 +0200

    add back tidyxl

--- process_scenario_weo_2022.R
+++ process_scenario_weo_2022.R
@@ -71,6 +71,13 @@ stopifnot(fs::file_exists(weo_2022_electric_sales_aps_auto_raw_full_filename))
 
 logger::log_info("WEO 2022: Loading WEO 2022 raw data.")
 
+read_xlsx_and_formats <- function(path, ...) {
+  cells <- tidyxl::xlsx_cells(path, ...)
+  formats <- tidyxl::xlsx_formats(path)
+  attr(cells, "formats") <- formats
+  cells
+}
+
 weo_2022_ext_data_regions_raw <-
   readr::read_csv(
     file = weo_2022_ext_data_regions_raw_full_filepath,
